a713d696e32963cfe615ae3507072ebb88ec36f7
VijayNamboodiri <vijay.namboodiri@hgs.hiddengeniusproject.org>
2021-11-17 01:41:29 +0000

    variables repl, talks about kpi's

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String name = "Vijay Namboodiri";
+    String deadLines = " Knowing what my dead lines are, and dividing work properly";
+    String stayingFocused = " Staying on task, but also taking breaks when necesarry";
+    String KPIMetrics = "number of customers, social media shares, and transactions made";
+    System.out.println("Hello my name is " + name      +", the time management strategy I would like to be held accountable"
+     + "to is" + deadLines + ", I will personally comit to" + deadLines
+        + "My business main KPI is" + stayingFocused + ","+ deadLines );
+  }
+}
